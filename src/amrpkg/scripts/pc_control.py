0c738e69d7cfe8c20e420841dba7c875021ff932
rafa <rfigueroa@unm.edu>
2015-02-13 09:40:53 -0700

    quad basics

---
+++ src/amrpkg/scripts/pc_control.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+from sensor_msgs.msg import PointCloud2
+import numpy as np
+
+
+
+def pc_callback(robot_pc):
+    pc = robot_pc.data
+    print pc
+
+    if False:
+        r = 1.0
+        k = 0.5
+        v = - k * (r - x)
+
+        robot_tw = Twist()
+        robot_tw.linear.x = v
+        pub.publish(robot_tw)
+
+def control():
+    rospy.init_node('amr_control')
+    rospy.Subscriber('/camera/depth/points', PointCloud2,
+                     pc_callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    pub = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=10)
+    control()
---
+++ src/amrpkg/scripts/quad_control.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Imu
+from gazebo_msgs.srv import GetModelState
+from hector_uav_msgs.msg import MotorPWM
+import numpy as np
+
+def control():
+    rospy.init_node('amr_control')
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    pub_pwm = rospy.Publisher('/motor_pwm', MotorPWM, queue_size=10)
+    rate = rospy.Rate(100)
+
+    rospy.wait_for_service('/gazebo/get_model_state/')
+    print 'service ready'
+
+    while not rospy.is_shutdown():
+
+        get_model_state = \
+        rospy.ServiceProxy('/gazebo/get_model_state/',
+                           GetModelState)
+        quad_state = get_model_state('quadrotor', 'world')
+        z = quad_state.pose.position.z
+        print 'z:', z
+
+        simple_controller = False
+
+        if simple_controller:
+            robot_tw = Twist()
+            robot_tw.linear.z = (1.4-z)*0.3
+            pub.publish(robot_tw)
+        else:
+            motor_pwm = MotorPWM()
+            k = 60
+            pwm = 40+int(k*(1.4-z))
+            print pwm
+            motor_pwm.pwm = [pwm, pwm, pwm, pwm]
+            pub_pwm.publish(motor_pwm)
+
+
+
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    control()
